c3eccfd0e78b97538b5b6112ee0921786e4c303b
Vikalp Nag <vikalpnag@gmail.com>
2024-02-27 23:58:17 +0530

    completed projects with functionality

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 let inputVal = document.querySelector(".middle input");
 let addBtn = document.querySelector("button");
-let Tasks = document.querySelector(".last");
+let Tasks = document.querySelector(".parent-last");
 
 addBtn.addEventListener("click", () => {
   taskAdding();
 });
 
+inputVal.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    taskAdding();
+  }
+});
+
+Tasks.addEventListener("click", (evt) => {
+  if (evt.target.classList.contains("fa-trash")) {
+    deleteTask(evt.target.closest(".last"));
+  }
+});
+
+//Task deleting function
+const deleteTask = (task) => {
+  task.remove();
+};
+
 //task adding into parent div
 const taskAdding = () => {
-
-    //Div 1 ************
+  let TaskContainer = document.createElement("div");
+  TaskContainer.classList.add("last");
+  //Div 1 ************
   let t1 = document.createElement("div");
   t1.classList.add("t1");
 
   let checkbox = document.createElement("input");
-  checkbox.type="checkbox";
+  checkbox.type = "checkbox";
   let paragraph = document.createElement("p");
-  paragraph.textContent = inputVal.value; 
+  paragraph.textContent = inputVal.value;
   t1.appendChild(checkbox);
   t1.appendChild(paragraph);
 
   //Div 2 ************
-  let icon=document.createElement("div");
+  let icon = document.createElement("div");
   icon.classList.add("icons");
 
-  let editIcon=document.createElement("i");
-  editIcon.classList.add("fa-solid","fa-pen-to-square");
-  let deleteIcon=document.createElement("i");
-  deleteIcon.classList.add("fa-solid","fa-trash");
-  icon.appendChild(editIcon);
-  icon.appendChild(deleteIcon)
+  let deleteIcon = document.createElement("i");
+  deleteIcon.classList.add("fa-solid", "fa-trash");
+  icon.appendChild(deleteIcon);
 
   //Adding divs  to main last or parent div
-  Tasks.appendChild(t1);
-  Tasks.appendChild(icon);
+  TaskContainer.appendChild(t1);
+  TaskContainer.appendChild(icon);
+
+  //adding to grandparent div
+  Tasks.appendChild(TaskContainer);
 };
